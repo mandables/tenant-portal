8c2ab0c62a20d2fe50c6ac713b6c45519a909ed7
amaan19 <amaan19@hotmail.com>
2020-01-18 16:29:21 +0000

    Added form for issue length

--- tenant-portal/src/components/ProblemForm.js
+++ tenant-portal/src/components/ProblemForm.js
@@ -10,6 +10,27 @@ export default class ProblemForm extends Component {
 					<option>Gas leak</option>
 					<option>General repairs</option>
 				</select>
+				How long has this issue been going on for?
+				<form>
+					<div className="radio">
+						<label>
+							<input type="radio" value="option1" />
+							Started today
+						</label>
+					</div>
+					<div className="radio">
+						<label>
+							<input type="radio" />
+							Under a week
+						</label>
+					</div>
+					<div className="radio">
+						<label>
+							<input type="radio" />
+							More than a week 
+						</label>
+					</div>
+				</form>
 			</div>
 		);
 	}
